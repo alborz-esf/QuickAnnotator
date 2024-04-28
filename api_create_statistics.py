4fb63a7a5b625aa690b3d2abfe3b9da1325d9cd2
alborz-esf <alborz.esf@gmail.com>
2024-04-28 09:07:04 +0330

    small changes

--- api_create_statistics.py
+++ api_create_statistics.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from skimage.measure import regionprops_table
 
-def read_images(api_img, api_mask):
+def read_images(api_img, api_mask, mask_filter=150, resize=480):
     img = None
     mask = None
 
@@ -14,14 +14,15 @@ def read_images(api_img, api_mask):
 
     img = img.convert('L')
     w, h = img.size
-    resize_factor = max(w,h)/480
+    resize_factor = max(w,h) / resize
+
     img = img.resize((int(w//resize_factor),int(h//resize_factor)))
     img = np.array(img)
 
     mask = mask.convert('L')
     mask = mask.resize((int(w//resize_factor),int(h//resize_factor)))
     mask = np.array(mask)
-    mask = mask > 150
+    mask = mask > mask_filter
     print(f'=============> images loaded')
     
     return make_prop(img, mask)
@@ -39,7 +40,6 @@ def make_table_of_features(labels):
     props_pd = regionprops_table(labels, properties=('centroid','orientation',
                                                      'eccentricity', 'perimeter', 'area'))
                                             
-    # return props_pd
     res_df = pd.DataFrame(props_pd)
     print(f'=============> table created')
     return res_df
